bf0e21af7a49cefd4dbd56c024f84913a3746a8f
HanDJ00 <han06225@gmail.com>
2024-10-22 17:51:29 +0900

    게시판 디자인 제외 틀

--- board/src/App.vue
+++ board/src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import Header from "@/header/Header.vue";
-import CategoryList from './category/view/CategoryList.vue';
+import CategoryList from './category/view/CategoryListView.vue';
 
 </script>
 
@@ -18,33 +18,3 @@ import CategoryList from './category/view/CategoryList.vue';
 /* 전역 스타일을 여기에 추가 */
 </style>
 
-<!-- App.vue -->
-<!--<template>-->
-<!--  <div id="app">-->
-<!--    &lt;!&ndash; 작성한 게시판 컴포넌트 표시 &ndash;&gt;-->
-<!--    <Board/>-->
-<!--  </div>-->
-<!--</template>-->
-
-<!--<script>-->
-<!--  import Board from './components/Board.vue';  -->
-
-<!--export default {-->
-<!--  name: 'App',-->
-<!--  components: {-->
-<!--    Board,-->
-<!--  },-->
-<!--};-->
-<!--</script>-->
-
-<!--<style>-->
-<!--/* 기본 스타일 설정 */-->
-<!--#app {-->
-<!--  font-family: Avenir, Helvetica, Arial, sans-serif;-->
-<!--  -webkit-font-smoothing: antialiased;-->
-<!--  -moz-osx-font-smoothing: grayscale;-->
-<!--  text-align: center;-->
-<!--  color: #2c3e50;-->
-<!--  margin-top: 60px;-->
-<!--}-->
-<!--</style>-->
