e12ae487df86f279418a86676a831e771060fee2
dispear <kkm7hjh@naver.com>
2024-10-26 19:33:41 +0900

    feat 각자의 파일 지움

--- chatting/src/main.js
+++
@@ -1,6 +0,0 @@
-import './assets/chatting.css'
-
-import { createApp } from 'vue'
-import App from './App.vue'
-
-createApp(App).mount('#app')
--- counseling/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from '@/router/router.js'
-import 'es6-promise/auto';
-
-const app = createApp(App);
-app.use(router);
-app.mount('#app')
